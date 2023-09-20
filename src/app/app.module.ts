79d326454b773d0a11a088a15424661d748ba32f
CleberAGallo <cleber.gallo@etec.sp.gov.br>
2023-09-19 22:20:00 -0300

    19-09

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,34 @@ import { HomeComponent } from './pages/home/home.component';
 import { SearchComponent } from './pages/search/search.component';
 import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
 
-import { HttpClientModule } from '@angular/common/http'; 
-import { MovieApiService } from './services/movie-api.service'; 
+import { HttpClientModule } from '@angular/common/http';
+import { MovieApiService } from './services/movie-api.service';
+
+import { DomSanitizer } from "@angular/platform-browser";
+import { Pipe, PipeTransform} from "@angular/core";
+import { ReactiveFormsModule } from '@angular/forms'
+
+@Pipe({ name: 'safe' })
+export class SafePipe implements PipeTransform {
+  constructor(private sanitizer: DomSanitizer) { }
+  transform(url: string) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+}
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SearchComponent,
-    MovieDetailsComponent
+    MovieDetailsComponent,
+    SafePipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [
     MovieApiService
--- src/app/services/movie-api.service.ts
+++ src/app/services/movie-api.service.ts
@@ -1,23 +1,78 @@
-import { HttpClient } from '@angular/common/http'; 
-import { Injectable } from '@angular/core'; 
-import { Observable } from 'rxjs'; 
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
-@Injectable({ 
-    providedIn: 'root' 
-  }) 
-  export class MovieApiService { 
+@Injectable({
+    providedIn: 'root'
+  })
+  export class MovieApiService {
 
-  constructor(private http: HttpClient) { } 
-  baseUrl = "https://api.themoviedb.org/3"; 
-  apiKey = "477a60aa738b2c4f3f466b0e56104046"; 
+  constructor(private http: HttpClient) { }
+  baseUrl = "https://api.themoviedb.org/3";
+  apiKey = "477a60aa738b2c4f3f466b0e56104046";
 
-    // banner API Data 
-    bannerApiData(): Observable<any> { 
-      return this.http.get(`${this.baseUrl}/trending/all/week?api_key=${this.apiKey}&language=pt-BR`); 
+    // banner API Data
+    bannerApiData(): Observable<any> {
+      return this.http.get(`${this.baseUrl}/trending/all/week?api_key=${this.apiKey}&language=pt-BR`);
     }
-    
-     // trending Movies API Data 
-    trendingMovieApiData(): Observable<any> { 
-      return this.http.get(`${this.baseUrl}/trending/movie/day?api_key=${this.apiKey}&language=pt-BR`); 
-    } 
-} 
+
+     // trending Movies API Data
+    trendingMovieApiData(): Observable<any> {
+      return this.http.get(`${this.baseUrl}/trending/movie/day?api_key=${this.apiKey}&language=pt-BR`);
+    }
+
+     // Movie Details API Data
+    movieDetails(data: any): Observable<any> {
+      return this.http.get(`${this.baseUrl}/movie/${data}?api_key=${this.apiKey}&language=pt-BR`);
+    }
+
+     // Movie Trailer API Data
+    movieVideo(data: any): Observable<any> {
+      return this.http.get(`${this.baseUrl}/movie/${data}/videos?api_key=${this.apiKey}&language=pt- BR`);
+    }
+
+    // Movie Cast API Data
+    movieCast(data: any): Observable<any> {
+      return this.http.get(`${this.baseUrl}/movie/${data}/credits?api_key=${this.apiKey}&language=pt- BR`);
+    }
+
+    // search Movie API Data
+    searchMovie(data: any): Observable<any> {
+      return this.http.get(`${this.baseUrl}/search/movie?api_key=${this.apiKey}&query=${data.movieName} &language=pt-BR`);
+    }
+
+    // Action Movies
+    fetchActionMovies(): Observable<any> {
+      return this.http.get(`${this.baseUrl}/discover/movie?api_key=${this.apiKey}&with_genres=28&langua ge=pt-BR`);
+    }
+
+    // Adventure Movies
+    fetchAdventureMovies(): Observable<any> {
+      return this.http.get(`${this.baseUrl}/discover/movie?api_key=${this.apiKey}&with_genres=12&language=pt-BR`);
+    }
+
+    // Animation Movies
+    fetchAnimationMovies(): Observable<any> {
+      return this.http.get(`${this.baseUrl}/discover/movie?api_key=${this.apiKey}&with_genres=16&language=pt-BR`);
+    }
+
+    // Comedy Movies
+    fetchComedyMovies(): Observable<any> {
+      return this.http.get(`${this.baseUrl}/discover/movie?api_key=${this.apiKey}&with_genres=35&language=pt-BR`);
+    }
+
+    // Documentary Movies
+    fetchDocumentaryMovies(): Observable<any> {
+      return this.http.get(`${this.baseUrl}/discover/movie?api_key=${this.apiKey}&with_genres=99&language=pt-BR`);
+    }
+
+    // Science-Fiction Movies
+    fetchScienceFictionMovies(): Observable<any> {
+      return this.http.get(`${this.baseUrl}/discover/movie?api_key=${this.apiKey}&with_genres=878&language=pt-BR`);
+    }
+
+    // Thriller Movies
+    fetchThrillerMovies(): Observable<any> {
+      return this.http.get(`${this.baseUrl}/discover/movie?api_key=${this.apiKey}&with_genres=53&language=pt-BR`);
+    }
+}
